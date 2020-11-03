5972f9d90e92b3f4de709e785e6461ac7f825144
Cédric Merliot <49645066+cedmer@users.noreply.github.com>
2020-11-03 15:46:03 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,13 +2,13 @@ pipeline{
   agent any
   tools {
         maven 'maven'
+        bat ' mvn -f pom.xml clean install'
     }
     stages {
       stage('Compile Stage'){
         steps{
           withMaven(maven : 'maven'){
             sh 'mvn clean compile'
-            bat ' mvn -f pom.xml clean install'
           }
         }
       }
